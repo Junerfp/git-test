4aa3f68609d51424aeec1731eaff934fab06ae90
June <June@qq.com>
2022-12-21 21:12:25 +0800

    hot-fix commit

--- src/main/java/com/softeem/test.java
+++ src/main/java/com/softeem/test.java
@@ -10,5 +10,6 @@ public class test {
         System.out.println("1111");
         System.out.println("2222");
         System.out.println("3333");
+        System.out.println("hot-fix test");
     }
 }
