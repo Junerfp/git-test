db2eb9893c8b8c8e675184748762ca268d1cdf5c
June <June@qq.com>
2022-12-21 21:08:46 +0800

    second commit

---
+++ src/main/java/com/softeem/test.java
@@ -0,0 +1,13 @@
+package com.softeem;
+
+/**
+ * @package com.softeem
+ * Author:rfp
+ * @Date:2022/12/21 21:00)
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("2222");
+    }
+}
